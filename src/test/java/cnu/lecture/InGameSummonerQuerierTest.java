e015c8ff997724de922267aedde8b75d7dc8c24d
rharnls72 <fnladl72@naver.com>
2016-05-01 15:42:54 +0900

    modify shouldQuerierIdentifyGameKeyWhenSpecificSummonerNameIsGiven

--- src/test/java/cnu/lecture/InGameSummonerQuerierTest.java
+++ src/test/java/cnu/lecture/InGameSummonerQuerierTest.java
@@ -6,19 +6,26 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.*;
+import cnu.lecture.InGameInfo.Observer;
+import cnu.lecture.InGameInfo.Participant;
 
 /**
  * Created by tchi on 2016. 4. 25..
  */
 public class InGameSummonerQuerierTest {
     private InGameSummonerQuerier querier;
-
+    private InGameInfo gameinfo;
+    private Observer observer;
+    private Participant[] participants;
     @Before
     public void setup() {
         final String apiKey = "8242f154-342d-4b86-9642-dfa78cdb9d9c";
         GameParticipantListener dontCareListener = mock(GameParticipantListener.class);
 
-        querier = new InGameSummonerQuerier(apiKey, dontCareListener);
+        querier = mock(InGameSummonerQuerier.class);
+        gameinfo = mock(InGameInfo.class);
+        observer = mock(Observer.class);
+        when(gameinfo.getObservers()).thenReturn(observer);
     }
 
     @Test
@@ -31,6 +38,7 @@ public class InGameSummonerQuerierTest {
 
         final String actualGameKey;
         WHEN: {
+            when(querier.queryGameKey(summonerName)).thenReturn("4/bl4DC8HBir8w7bGHq6hvuHluBd+3xM");
             actualGameKey = querier.queryGameKey(summonerName);
         }
 
